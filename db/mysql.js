41223a9b00bbf8d4cae695a400f8889550b93e79
Hilor Jain <hilor05121998@gmail.com>
2023-06-14 23:26:20 +0530

    added db connection

---
+++ db/mysql.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2');
+const { createPool } = require('mysql2');
+const config = require('config');
+
+exports.createMysqlConn = ()=>{
+    console.log("connecting to mysql");
+    try {
+        const pool = mysql.createPool({
+            host: config.host,        
+            user: config.user,     
+            password: config.password,
+            database: config.database,
+            connectionLimit: config.connectionLimit,
+        }).promise();
+    } catch(err) {
+        console.log(err);
+    }
+      return pool;    
+};
+
+exports.execQuery = async(query, params) => {
+    try{
+        const result = await pool.query(query, params);
+    } catch(err) {
+        console.log(err);    
+    }
+    return result;
+}  
+
+  
+  
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const loginRoutes = require('./routes/loginRoutes');
-
+const {getMsqlConn} = require('./db/mysql');
 
